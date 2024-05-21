849e4fef9e8bc6b148d5bd25679fe402e6cd0806
scottww <798868678@qq.com>
2024-05-21 15:06:13 +0800

    feat: slider update

--- src/views/slider/index.vue
+++ src/views/slider/index.vue
@@ -1,39 +1,148 @@
 <template>
-  <div>
-    <el-slider
-      v-model="sliderValue"
-      :min="0"
-      :max="totalTime"
-      :step="1"
-      :show-tooltip="false"
-      @change="handleSliderChange"
-    ></el-slider>
-    <p>{{ currentTime }}</p>
-    <el-button @click="toFirst" :disabled="sliderValue === 0">first</el-button>
-    <el-button @click="prevFrame" :disabled="sliderValue === 0">prev</el-button>
-    <el-button @click="nextFrame" :disabled="sliderValue === totalTime"
-      >next</el-button
-    >
-    <el-button @click="toLast" :disabled="sliderValue === 100">last</el-button>
-    <el-button @click="togglePlay">{{ playing ? "暂停" : "播放" }}</el-button>
+  <div class="slider-demo">
+    <div class="container">
+      <div class="left-panel">
+        <div class="top">
+          <div class="btn-change">
+            <el-button-group v-model="currentBtnValue">
+              <el-button
+                v-for="(item, index) in btnList"
+                :type="currentBtnValue === item.value ? 'primary' : ''"
+                size="mini"
+                :key="`btn-${index}`"
+                @click="changeBtn(item)"
+                >{{ item.text }}</el-button
+              >
+            </el-button-group>
+          </div>
+          <el-slider
+            v-model="sliderValue"
+            :min="0"
+            :max="totalTime"
+            :step="1"
+            :show-tooltip="false"
+            @change="handleSliderChange"
+          ></el-slider>
+          <p>{{ currentTime }}</p>
+          <el-button
+            size="mini"
+            @click="firstFrame"
+            :disabled="sliderValue === 0"
+            >第一帧</el-button
+          >
+          <el-button
+            size="mini"
+            @click="prevFrame"
+            :disabled="sliderValue === 0"
+            >上一帧</el-button
+          >
+          <el-button
+            size="mini"
+            @click="nextFrame"
+            :disabled="sliderValue === totalTime"
+            >下一帧</el-button
+          >
+          <el-button
+            size="mini"
+            @click="lastFrame"
+            :disabled="sliderValue === totalTime"
+            >最后一帧</el-button
+          >
+          <el-button size="mini" @click="togglePlay">{{
+            playing ? "暂停" : "播放"
+          }}</el-button>
+        </div>
+        <div class="bottom">
+          <div class="imageBlock">
+            <img src="@/assets/slider/1.png" alt="png" />
+          </div>
+        </div>
+      </div>
+
+      <div class="right-panel">
+        <ul>
+          <li
+            v-for="(item, index) in timeList"
+            :key="index"
+            :class="{ active: currentSegment === index }"
+          >
+            {{ item.value }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { Slider, Button } from "element-ui";
+// import { Slider, Button } from 'element-ui';
 
 export default {
-  components: {
-    "el-slider": Slider,
-    "el-button": Button
-  },
+  // components: {
+  //   'el-slider': Slider,
+  //   'el-button': Button
+  // },
   data() {
     return {
       totalTime: 100, // 总时间，单位可以根据需要调整
       sliderValue: 0, // 滑块当前值
       numDivisions: 10, // 将滑块分割为10等份
       playing: false, // 播放状态
-      currentSegment: 0 // 当前播放的时间段
+      currentSegment: 0, // 当前播放的时间段
+      currentBtnValue: 1,
+      btnList: [
+        {
+          text: "1小时降雨量",
+          value: 1
+        },
+        {
+          text: "6小时降雨量",
+          value: 6
+        },
+        {
+          text: "12小时降雨量",
+          value: 12
+        },
+        {
+          text: "24小时降雨量",
+          value: 24
+        }
+      ],
+      timeList: [
+        {
+          value: "05-21 08:00"
+        },
+        {
+          value: "05-21 09:00"
+        },
+        {
+          value: "05-21 10:00"
+        },
+        {
+          value: "05-21 11:00"
+        },
+        {
+          value: "05-21 12:00"
+        },
+        {
+          value: "05-21 13:00"
+        },
+        {
+          value: "05-21 14:00"
+        },
+        {
+          value: "05-21 15:00"
+        },
+        {
+          value: "05-21 16:00"
+        },
+        {
+          value: "05-21 17:00"
+        },
+        {
+          value: "05-21 18:00"
+        }
+      ]
     };
   },
   computed: {
@@ -43,7 +152,12 @@ export default {
     }
   },
   methods: {
+    changeBtn(item) {
+      console.log("changeBtn ...", item);
+      this.currentBtnValue = item.value;
+    },
     handleSliderChange(value) {
+      console.log("handleSliderChange ...", value);
       // 计算当前播放的时间段
       this.currentSegment = Math.floor(
         value / (this.totalTime / this.numDivisions)
@@ -63,7 +177,7 @@ export default {
             this.playSegment(segment + 1);
           }
         }
-      }, 500); // this.totalTime / this.numDivisions) * 1000根据时间段长度计算延迟时间
+      }, 500); // (this.totalTime / this.numDivisions) * 1000根据时间段长度计算延迟时间
     },
     togglePlay() {
       // 切换播放状态
@@ -72,29 +186,34 @@ export default {
         this.playSegment(this.currentSegment);
       }
     },
-    prevFrame() {
-      // 上一针操作
+    firstFrame() {
+      console.log("firstFrame ...", this);
       this.playing = false;
+      // 跳转到第一帧
+      this.sliderValue = 0;
+      this.currentSegment = 0;
+    },
+    lastFrame() {
+      this.playing = false;
+      // 跳转到最后一帧
+      this.sliderValue = this.totalTime;
+      this.currentSegment = this.numDivisions;
+    },
+    prevFrame() {
+      this.playing = false;
+      // 上一针操作
       if (this.sliderValue > 0) {
         this.sliderValue -= this.totalTime / this.numDivisions;
         this.currentSegment -= 1;
       }
     },
     nextFrame() {
-      // 下一针操作
       this.playing = false;
+      // 下一针操作
       if (this.sliderValue < this.totalTime) {
         this.sliderValue += this.totalTime / this.numDivisions;
         this.currentSegment += 1;
       }
-    },
-    toFirst() {
-      this.playing = false;
-      this.sliderValue = 0;
-    },
-    toLast() {
-      this.playing = false;
-      this.sliderValue = 100;
     }
   },
   mounted() {
@@ -106,6 +225,43 @@ export default {
 };
 </script>
 
-<style scoped>
-/* 在这里添加自定义样式 */
+<style lang="scss" scoped>
+.slider-demo {
+  .container {
+    display: flex;
+  }
+  .left-panel {
+    flex: 7;
+    padding: 0 10px;
+    .top {
+      border-bottom: 1px solid #ddd;
+      padding: 10px;
+    }
+    .bottom {
+      //图片区
+      .imageBlock img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+  .right-panel {
+    flex: 3;
+    li {
+      border-bottom: 1px solid #ddd;
+      padding: 4px 0;
+      cursor: pointer;
+      &:hover {
+        background-color: #1890ff;
+        color: #fff;
+      }
+    }
+  }
+
+  .active {
+    background-color: #1890ff;
+    color: #fff;
+  }
+}
 </style>
