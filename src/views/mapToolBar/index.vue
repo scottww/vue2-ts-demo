864cd6b44c7a57b8565a2955aa7d04042fdce84f
scottww <wb-huangshuifa@yunlizhihui.onaliyun.com>
2025-02-20 13:55:41 +0800

    feat: maptoolbar selected

--- src/views/mapToolBar/index.vue
+++ src/views/mapToolBar/index.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="tools-box">
-    <div class="tools-box_item" @mouseover="hover = true" @mouseleave="hover = false">
+    <div class="tools-box_item" @click="selectTool('add')" title="新增">
       <!-- <img class="img-box" :src="require('@/assets/images/mapToolBar/add.png')" alt="" /> -->
       <!-- <img class="img-box" src="@/assets/mapToolBar/add.png" alt="" /> -->
-      <img class="img-box" :src="hover ? hoverImage : defaultImage" alt="动态图片" />
+      <img
+        class="img-box"
+        :src="selectedTool === 'add' ? activeImage : defaultImage"
+        alt="动态图片"
+      />
+    </div>
+    <div class="tools-box_item" @click="selectTool('edit')" title="编辑">
+      <img
+        class="img-box"
+        :src="selectedTool === 'edit' ? activeImageEdit : defaultImageEdit"
+        alt="动态图片"
+      />
     </div>
   </div>
 </template>
 
 <script>
-const defaultImage = require('@/assets/mapToolBar/add_grey.png');  // 默认图片
-const hoverImage = require('@/assets/mapToolBar/add.png');  // 悬停时的图片
+const defaultImage = require("@/assets/mapToolBar/add_grey.png"); // 默认图片
+const activeImage = require("@/assets/mapToolBar/add.png"); // 悬停时的图片
+const defaultImageEdit = require("@/assets/mapToolBar/edit_grey.png"); // 默认图片
+const activeImageEdit = require("@/assets/mapToolBar/edit.png"); // 悬停时的图片
 
 export default {
-  data(){
+  data() {
     return {
       defaultImage,
-      hoverImage,
-      hover: false
+      activeImage,
+      defaultImageEdit,
+      activeImageEdit,
+      hover: false,
+      selectedTool: ""
+    };
+  },
+  methods: {
+    selectTool(tool) {
+      console.log("selectTool", tool);
+      // 切换选中的工具，确保只有一个工具被选中
+      this.selectedTool = this.selectedTool === tool ? "" : tool;
     }
   }
 };
