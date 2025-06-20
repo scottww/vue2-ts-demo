2e7d00c500df343168e73f730f6977fdc3894d13
scottww <wb-huangshuifa@yunlizhihui.onaliyun.com>
2025-06-20 11:04:54 +0800

    feat: map_done delete feature show confirm

--- src/views/map2D/index_done.vue
+++ src/views/map2D/index_done.vue
@@ -1132,8 +1132,15 @@ export default {
         this.featureIdToClose[id] = close;
 
         close.addEventListener("click", () => {
-          this.savedSource.removeFeature(feature);
-          overlaysForThisFeature.forEach((o) => this.map.removeOverlay(o));
+          //删除前 确认框
+          this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+          }).then(() => {
+            this.savedSource.removeFeature(feature);
+            overlaysForThisFeature.forEach((o) => this.map.removeOverlay(o));
+          });
         });
 
         const closeOverlay = new Overlay({
